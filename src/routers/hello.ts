43d6e239bd5a7504de5637db730a0833c0c18873
Francisco Ramos <fro.profesional@gmail.com>
2023-04-04 15:15:06 -0700

    Init api rest

---
+++ src/routers/hello.ts
@@ -0,0 +1,23 @@
+import { ApiRouter } from "../../deps.ts";
+import config from "../config.ts";
+import { hello as helloMiddleware } from "../middlewares/hello.ts";
+
+// Extract name from config
+const { name } = config;
+
+// Create a new router instance
+const hello = new ApiRouter();
+
+// Add routes
+hello.all("/", helloMiddleware, async (ctx) => {
+  const { logger } = ctx.app.state;
+
+  logger.info("Hello from route");
+
+  ctx.response.body = {
+    message: `Hello from ${name}`,
+  };
+});
+
+// Export the router instance
+export { hello };
